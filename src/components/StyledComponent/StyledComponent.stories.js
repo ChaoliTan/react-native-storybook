17605bb66d00106bc3e717b689be9214309258cd
ChaoliTan <ctan7749@gmail.com>
2021-05-04 12:09:40 -0700

    delete test storybook components

--- src/components/StyledComponent/StyledComponent.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react-native'
-
-import StyledComponent from './StyledComponent'
-
-storiesOf('Styled Component', module).add('example', () => <StyledComponent />)
--- src/components/StyledPressable/StyledPressable.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react-native'
-
-import StyledPressable from './StyledPressable'
-
-storiesOf('Styled Pressable', module).add('example', () => <StyledPressable />)
