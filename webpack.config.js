24f519c0b561f251a20616ceb06b40eb10dee7a6
jankergg <jankergg@gmail.com>
2021-05-12 11:54:58 -0700

    add editconfigs

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: path.resolve(__dirname, './public/index.html'),
-  filename: 'index.html',
-  inject: 'body',
-})
+    template: path.resolve(__dirname, './public/index.html'),
+    filename: 'index.html',
+    inject: 'body',
+});
 
 module.exports = {
-  entry: path.join(__dirname, 'index.web.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/build'),
-  },
-  resolve: {
-    alias: {
-      'react-native$': 'react-native-web',
-      '@storybook/react-native': '@storybook/react',
-      'styled-components/native': 'styled-components',
+    entry: path.join(__dirname, 'index.web.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, '/build'),
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)?$/,
-        exclude: /node_modules\/(?!()\/).*/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-          },
+    resolve: {
+        alias: {
+            'react-native$': 'react-native-web',
+            '@storybook/react-native': '@storybook/react',
+            'styled-components/native': 'styled-components',
         },
-      },
-    ],
-  },
-  plugins: [HTMLWebpackPluginConfig],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    hot: true,
-  },
-}
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)?$/,
+                exclude: /node_modules\/(?!()\/).*/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [HTMLWebpackPluginConfig],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './',
+        hot: true,
+    },
+};
